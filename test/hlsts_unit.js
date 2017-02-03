6a894fd410122faa060261fe6e3b4e0db57bbfa6
Jonas Birme <jonas.birme@gmail.com>
2017-02-03 22:52:55 +0100

    Disabled test until setup is completed

--- test/hlsts_unit.js
+++ test/hlsts_unit.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const hlsTs = require("../index.js");
 
-describe("Hls TS", () => {
+xdescribe("Hls TS", () => {
   it("can parse data through a pipe", (done) => {
     request
     .get("http://localhost:9876/base/test/support/testassets/seg-10s.ts")
