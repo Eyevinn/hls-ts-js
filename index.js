fc8eca7b4c17615a7ab1fcb864f32ab5287726e6
Jonas Birme <jonas.birme@gmail.com>
2017-02-03 21:45:31 +0100

    Committed main JS file

---
+++ index.js
@@ -0,0 +1,10 @@
+// Copyright 2017 Eyevinn Technology. All rights reserved
+// Use of this source code is governed by a MIT License
+// license that can be found in the LICENSE file.
+// Author: Jonas Birme (Eyevinn Technology)
+
+const HlsTS = function constructor() {
+
+};
+
+module.exports = HlsTS;
