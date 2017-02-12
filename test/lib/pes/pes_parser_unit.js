523392ffb6c046a75028f06b6d85dfceba9df1a4
Jonas Birme <jonas.birme@gmail.com>
2017-02-12 20:42:04 +0100

    Do not output debug output when running unit tests

--- test/lib/pes/pes_parser_unit.js
+++ test/lib/pes/pes_parser_unit.js
@@ -13,7 +13,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 describe("PES Parser", () => {
   it("can parse PES AVC payload", (done) => {
     const stream = request.get("http://localhost:9876/base/test/support/testassets/seg-10s.ts");
-    stream.pipe(hlsTs.parse({ debug: true })).on("finish", () => {
+    stream.pipe(hlsTs.parse({ debug: false })).on("finish", () => {
       const avcData = hlsTs.getDataStreamByProgramType("avc");
       const pesAvcParser = new PESAVCParser(avcData);
       expect(pesAvcParser.getId()).toBe(224);
